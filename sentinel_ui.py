7e9a23ecb40e5837cfb6927277d0f14dd93a3fac
Sri Naga Vijay Kethe <kethesrinagavijay@gmail.com>
2026-06-27 18:48:11 +0530

    Fix page_title formatting in sentinel_ui.py

--- sentinel_ui.py
+++ sentinel_ui.py
@@ -6,7 +6,7 @@ from engine import analyze_decision
 # --------------------------------------------------
 
 st.set_page_config(
-    page_title="st.write("UI VERSION V3")",
+    page_title="UI VERSION V3",
     page_icon="🛡️",
     layout="wide"
 )
